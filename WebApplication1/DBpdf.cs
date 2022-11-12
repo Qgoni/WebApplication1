316e395742dba0c24ee995dc66383f782af146d1
Qgoni <pasword20.secure@gmail.com>
2022-11-12 16:10:34 +0300

    Initial commit

---
+++ WebApplication1/DBpdf.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+using DbContext = System.Data.Entity.DbContext;
+
+namespace WebApplication1;
+
+public class DBpdf : DbContext
+{
+  public DBpdf(DbContextOptions<DBpdf> options)
+    : base(options)
+  {
+  }
+
+  public System.Data.Entity.DbSet<Files> Files { get; set; }
+
+  protected override void OnConfiguiring(DbContextOptionsBuilder optionsBuilder)
+  {
+    optionsBuilder.UseSqlServer(@"DESKTOP-0I5TVMN\SQLEXPRESS");
+
+  }
+}
---
+++ WebApplication1/Models/Files.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models;
+
+[Table("Files")]
+public class Files
+{
+    [Key] 
+    public long id { get; set; }
+    public string Name { get; set; }
+    public byte Pdf { get; set; }
+    
+}
